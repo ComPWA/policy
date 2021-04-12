214ba420dbfc98669fa6a4a04e273dd6f141c7e4
Remco de Boer <redeboer@gmx.com>
2021-04-12 09:04:56 +0200

    fix: do not pass filenames to check-dev-files

--- src/repoma/pre_commit_hooks/check_dev_files/__init__.py
+++ src/repoma/pre_commit_hooks/check_dev_files/__init__.py
@@ -13,7 +13,6 @@ from .editor_config_hook import check_editor_config_hook
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("filenames", nargs="*", help="Dummy for pre-commit")
     parser.add_argument(
         "--no-fix",
         default=False,
