bb3a47fd0a0141a8327912ab883c075b6b3cd0b7
Remco de Boer <redeboer@gmx.com>
2021-05-17 11:20:40 +0200

    feat: update requirements workflows

--- src/repoma/pre_commit_hooks/check_dev_files/pin_requirements_scripts.py
+++ src/repoma/pre_commit_hooks/check_dev_files/pin_requirements_scripts.py
@@ -10,26 +10,33 @@ __THIS_MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 __EXPECTED_SCRIPT_PATH = os.path.abspath(
     f"{__THIS_MODULE_DIR}/../../devtools/{__SCRIPT_NAME}"
 )
-with open(__EXPECTED_SCRIPT_PATH) as STREAM:
-    __EXPECTED_SCRIPT = STREAM.read()
 
 
 def check_constraints_folder() -> None:
     update_pin_requirements_script()
     remove_bash_script()
+    update_github_workflows()
 
 
 def update_pin_requirements_script() -> None:
+    with open(__EXPECTED_SCRIPT_PATH) as stream:
+        expected_script = stream.read()
     if not os.path.exists(f"{__CONSTRAINTS_DIR}/{__SCRIPT_NAME}"):
-        write_script()
+        write_script(
+            content=expected_script,
+            path=f"{__CONSTRAINTS_DIR}/{__SCRIPT_NAME}",
+        )
         raise PrecommitError(
             f'This repository does not contain a "{__SCRIPT_NAME}" script.'
             " Problem has been fixed"
         )
     with open(f"{__CONSTRAINTS_DIR}/{__SCRIPT_NAME}") as stream:
         existing_script = stream.read()
-    if existing_script != __EXPECTED_SCRIPT:
-        write_script()
+    if existing_script != expected_script:
+        write_script(
+            content=expected_script,
+            path=f"{__CONSTRAINTS_DIR}/{__SCRIPT_NAME}",
+        )
         raise PrecommitError(f'Updated "{__SCRIPT_NAME}" script')
 
 
@@ -40,6 +47,30 @@ def remove_bash_script() -> None:
         raise PrecommitError(f'Removed deprecated "{bash_script_name}" script')
 
 
-def write_script() -> None:
-    with open(f"{__CONSTRAINTS_DIR}/{__SCRIPT_NAME}", "w") as stream:
-        stream.write(__EXPECTED_SCRIPT)
+def update_github_workflows() -> None:
+    github_workflow_dir = ".github/workflows"
+
+    def upgrade_workflow(workflow_file: str) -> None:
+        expected_workflow_path = os.path.abspath(
+            f"{__THIS_MODULE_DIR}/../../{workflow_file}"
+        )
+        with open(expected_workflow_path) as stream:
+            expected_content = stream.read()
+        workflow_path = f"{github_workflow_dir}/{workflow_file}"
+        if not os.path.exists(workflow_path):
+            write_script(expected_content, path=workflow_path)
+            raise PrecommitError(f'Created "{workflow_path}" workflow')
+
+        with open(workflow_path) as stream:
+            existing_content = stream.read()
+        if existing_content != expected_content:
+            write_script(expected_content, path=workflow_path)
+            raise PrecommitError(f'Updated "{workflow_path}" workflow')
+
+    upgrade_workflow("requirements-cron.yml")
+    upgrade_workflow("requirements-pr.yml")
+
+
+def write_script(content: str, path: str) -> None:
+    with open(path, "w") as stream:
+        stream.write(content)
