e9dce73209813b43c8ebd4c5ad30e7d88b56185a
Remco de Boer <redeboer@gmx.com>
2022-01-16 20:49:16 +0100

    feat: split and simplify notebook install cells (#44)
    * docs: automatically determine replace cell
    * docs: remove capture and jedi cell statement
    * docs: split config and install cell
    * docs: simplify install cell
    * feat: add option to install extra packages
    * fix: remove doc from default extras require
    * fix: remove graphviz from default installed packages
    * fix: remove replace and colab argument setup.cfg format
    * refactor: rename colab argument to add-install-cell

--- src/repoma/fix_first_nbcell.py
+++ src/repoma/fix_first_nbcell.py
@@ -1,12 +1,12 @@
 """Add install statements to first cell in a Jupyter notebook.
 
-Google Colaboratory does not install a package automatically, so this has to be
-done through a code cell. At the same time, this cell needs to be hidden from
-the documentation pages, when viewing through Jupyter Lab (Binder), and when
-viewing Jupyter slides.
+Notebook servers like Google Colaboratory and Deepnote do not install a package
+automatically, so this has to be done through a code cell. At the same time,
+this cell needs to be hidden from the documentation pages, when viewing through
+Jupyter Lab (Binder), and when viewing as Jupyter slides.
 
-Additionally, this scripts sets the IPython InlineBackend.figure_formats option
-to SVG. This is because the Sphinx configuration can't set this externally.
+This scripts sets the IPython InlineBackend.figure_formats option to SVG. This
+is because the Sphinx configuration can't set this externally.
 
 Notebooks can be ignored by making the first cell a `Markdown cell
 <https://jupyter-notebook.readthedocs.io/en/latest/examples/Notebook/Working%20With%20Markdown%20Cells.html>`_
@@ -27,30 +27,14 @@ from repoma.utilities.setup_cfg import open_setup_cfg
 
 __SETUP_CFG = open_setup_cfg()
 __PACKAGE_NAME = __SETUP_CFG["metadata"]["name"]
-__DEFAULT_CONTENT = """
-%%capture
-%config Completer.use_jedi = False
+
+__CONFIG_CELL_CONTENT = """
 %config InlineBackend.figure_formats = ['svg']
 import os
 
 STATIC_WEB_PAGE = {"EXECUTE_NB", "READTHEDOCS"}.intersection(os.environ)
 """
-__COLAB_CONTENT = f"""
-# Install on Google Colab
-import subprocess
-import sys
-
-from IPython import get_ipython
-
-install_packages = "google.colab" in str(get_ipython())
-if install_packages:
-    for package in ["{__PACKAGE_NAME}[doc]", "graphviz"]:
-        subprocess.check_call(
-            [sys.executable, "-m", "pip", "install", package]
-        )
-"""
-
-__EXPECTED_CELL_METADATA = {
+__CONFIG_CELL_METADATA: dict = {
     "hideCode": True,
     "hideOutput": True,
     "hidePrompt": True,
@@ -59,14 +43,78 @@ __EXPECTED_CELL_METADATA = {
     "tags": ["remove-cell"],
 }
 
+__INSTALL_CELL_CONTENT = f"""
+# WARNING: advised to install a specific version, e.g. {__PACKAGE_NAME}==0.1.2
+%pip install -q {__PACKAGE_NAME}
+"""
+__INSTALL_CELL_METADATA: dict = {
+    **__CONFIG_CELL_METADATA,
+    "tags": ["remove-cell", "skip-execution"],
+    # https://github.com/executablebooks/jupyter-book/issues/833
+}
 
-def fix_first_cell(
-    filename: str, new_content: str, replace: bool = False
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
+    parser.add_argument(
+        "--add-install-cell",
+        action="store_true",
+        help="Add notebook cell with pip install statement.",
+    )
+    parser.add_argument(
+        "--extras-require",
+        default="",
+        help="Comma-separated list of optional dependencies, e.g. doc,viz",
+        type=str,
+    )
+    parser.add_argument(
+        "--additional-packages",
+        default="",
+        help=(
+            "Comma-separated list of packages that should be installed"
+            " with pip"
+        ),
+        type=str,
+    )
+    args = parser.parse_args(argv)
+
+    for filename in args.filenames:
+        _update_cell(
+            filename,
+            new_content=__CONFIG_CELL_CONTENT.strip("\n"),
+            new_metadata=__CONFIG_CELL_METADATA,
+            cell_id=0,
+        )
+        if args.add_install_cell:
+            cell_content = __INSTALL_CELL_CONTENT.strip("\n")
+            if args.extras_require:
+                extras = args.extras_require.strip()
+                cell_content += f"[{extras}]"
+            if args.additional_packages:
+                packages = map(
+                    lambda s: s.strip(), args.additional_packages.split(",")
+                )
+                cell_content += " " + " ".join(packages)
+            _update_cell(
+                filename,
+                new_content=cell_content,
+                new_metadata=__INSTALL_CELL_METADATA,
+                cell_id=1,
+            )
+    return 0
+
+
+def _update_cell(
+    filename: str,
+    new_content: str,
+    new_metadata: dict,
+    cell_id: int,
 ) -> None:
     notebook = nbformat.read(filename, as_version=nbformat.NO_CONVERT)
-    old_cell = notebook["cells"][0]
-    if old_cell["cell_type"] == "markdown":
-        old_cell_content: str = old_cell["source"]
+    exiting_cell = notebook["cells"][cell_id]
+    if exiting_cell["cell_type"] == "markdown":
+        old_cell_content: str = exiting_cell["source"]
         old_cell_content = old_cell_content.lower()
         first_line = old_cell_content.split("\n")[0]
         first_line = first_line.strip()
@@ -77,46 +125,18 @@ def fix_first_cell(
             and "cell" in first_line
         ):
             return
+
     new_cell = nbformat.v4.new_code_cell(
         new_content,
-        metadata=__EXPECTED_CELL_METADATA,
+        metadata=new_metadata,
     )
     del new_cell["id"]  # following nbformat_minor = 4
-    if replace:
-        notebook["cells"][0] = new_cell
+    if exiting_cell["cell_type"] == "code":
+        notebook["cells"][cell_id] = new_cell
     else:
-        notebook["cells"] = [new_cell] + notebook["cells"]
+        notebook["cells"].insert(cell_id, new_cell)
     nbformat.validate(notebook)
     nbformat.write(notebook, filename)
-
-
-def main(argv: Optional[Sequence[str]] = None) -> int:
-    parser = argparse.ArgumentParser(__doc__)
-    parser.add_argument("filenames", nargs="*", help="Filenames to check.")
-    parser.add_argument(
-        "--replace",
-        action="store_true",
-        help="Replace first cell instead of prepending a new cell.",
-    )
-    parser.add_argument(
-        "--colab",
-        action="store_true",
-        help="Add pip install statements for Google Colab.",
-    )
-    args = parser.parse_args(argv)
-
-    expected_cell_content = __DEFAULT_CONTENT.strip("\n")
-    if args.colab:
-        expected_cell_content += "\n\n"
-        expected_cell_content += __COLAB_CONTENT.strip("\n")
-    exit_code = 0
-    for filename in args.filenames:
-        fix_first_cell(
-            filename,
-            new_content=expected_cell_content,
-            replace=args.replace,
-        )
-    return exit_code
 
 
 if __name__ == "__main__":
