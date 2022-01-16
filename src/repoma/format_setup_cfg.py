5b0df2b0d448d725a8035876109149a5ad2688f5
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-01-16 12:26:17 +0100

    fix: remove replace and colab argument setup.cfg format

--- src/repoma/format_setup_cfg.py
+++ src/repoma/format_setup_cfg.py
@@ -49,16 +49,6 @@ def _format_setup_cfg(
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("filenames", nargs="*", help="Filenames to check.")
-    parser.add_argument(
-        "--replace",
-        action="store_true",
-        help="Replace first cell instead of prepending a new cell.",
-    )
-    parser.add_argument(
-        "--colab",
-        action="store_true",
-        help="Add pip install statements for Google Colab.",
-    )
     args = parser.parse_args(argv)
     if str(CONFIG_PATH.setup_cfg) in args.filenames:
         format_setup_cfg()
