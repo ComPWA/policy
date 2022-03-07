12832c905e892994a31448c0a51d18490fffe1cb
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-03-07 20:38:09 +0100

    feat: auto-detect pyupgrade argument

--- src/repoma/check_dev_files/pyupgrade.py
+++ src/repoma/check_dev_files/pyupgrade.py
@@ -2,12 +2,13 @@
 
 
 from repoma.errors import PrecommitError
-from repoma.utilities import CONFIG_PATH
+from repoma.utilities import CONFIG_PATH, natural_sorting
 from repoma.utilities.executor import Executor
 from repoma.utilities.precommit import (
     PrecommitConfig,
     load_round_trip_precommit_config,
 )
+from repoma.utilities.setup_cfg import get_supported_python_versions
 
 
 def main() -> None:
@@ -22,7 +23,7 @@ def _update_main_pyupgrade_hook() -> None:
     repo_url = "https://github.com/asottile/pyupgrade"
     hook_id = "pyupgrade"
     expected_args = [
-        "--py36-plus",
+        __get_pyupgrade_version_argument(),
     ]
     precommit_config = PrecommitConfig.load()
     repo = precommit_config.find_repo(repo_url)
@@ -66,7 +67,7 @@ def _update_nbqa_hook() -> None:
     expected_config = {
         "id": hook_id,
         "args": [
-            "--py36-plus",
+            __get_pyupgrade_version_argument(),
         ],
     }
     repo_index = precommit_config.get_repo_index(repo_url)
@@ -82,3 +83,17 @@ def _update_nbqa_hook() -> None:
         config["repos"][repo_index]["hooks"][hook_index] = expected_config
         yaml.dump(config, CONFIG_PATH.precommit)
         raise PrecommitError(f"Updated args of {hook_id} pre-commit hook")
+
+
+def __get_pyupgrade_version_argument() -> str:
+    """Get the --py3x-plus argument for pyupgrade.
+
+    >>> __get_pyupgrade_version_argument()
+    '--py36-plus'
+    """
+    supported_python_versions = sorted(
+        (v.replace(".", "") for v in get_supported_python_versions()),
+        key=natural_sorting,
+    )
+    lowest_version = supported_python_versions[0]
+    return f"--py{lowest_version}-plus"
