5b10f6b14bce0040d995307c7d87c1a07061e849
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-04-24 18:20:59 +0200

    feat: check SVG in notebook cell output

--- src/repoma/fix_nbformat_version.py
+++ src/repoma/fix_nbformat_version.py
@@ -6,35 +6,72 @@ diffs. The solution is to set the version to v4 and removes those cell ids.
 
 import argparse
 import sys
+from textwrap import dedent
 from typing import Optional, Sequence
 
 import nbformat
 
+from .errors import PrecommitError
+from .utilities.executor import Executor
 
-def set_nbformat_version(filename: str) -> None:
-    notebook = nbformat.read(filename, as_version=nbformat.NO_CONVERT)
-    if notebook["nbformat_minor"] != 4:
-        notebook["nbformat_minor"] = 4
-        nbformat.write(notebook, filename)
-
-
-def remove_cell_ids(filename: str) -> None:
-    notebook = nbformat.read(filename, as_version=nbformat.NO_CONVERT)
-    for cell in notebook["cells"]:
-        if "id" in cell:
-            del cell["id"]
-    nbformat.write(notebook, filename)
+BINARY_CELL_OUTPUT = [
+    "image/jpeg",
+    "image/png",
+    "image/svg+xml",
+]
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser(__doc__)
     parser.add_argument("filenames", nargs="*", help="Filenames to fix.")
     args = parser.parse_args(argv)
-    exit_code = 0
+    executor = Executor()
     for filename in args.filenames:
-        set_nbformat_version(filename)
-        remove_cell_ids(filename)
-    return exit_code
+        executor(set_nbformat_version, filename)
+        executor(remove_cell_ids, filename)
+        executor(check_svg_output_cells, filename)
+    if executor.error_messages:
+        print(executor.merge_messages())
+        return 1
+    return 0
+
+
+def set_nbformat_version(filename: str) -> None:
+    notebook = open_notebook(filename)
+    if notebook["nbformat_minor"] != 4:
+        notebook["nbformat_minor"] = 4
+        nbformat.write(notebook, filename)
+
+
+def remove_cell_ids(filename: str) -> None:
+    notebook = open_notebook(filename)
+    for cell in notebook["cells"]:
+        if "id" in cell:
+            del cell["id"]
+    nbformat.write(notebook, filename)
+
+
+def check_svg_output_cells(filename: str) -> None:
+    notebook = open_notebook(filename)
+    for i, cell in enumerate(notebook["cells"]):
+        for output in cell.get("outputs", []):
+            data = output.get("data", {})
+            for binary in BINARY_CELL_OUTPUT:
+                if binary in data:
+                    # pylint: disable=line-too-long
+                    error_message = f"""
+                    Cell {i} in {filename} contains {binary} output. Please
+                    store it somewhere outside this repository and render it
+                    through an external link. See for instance
+                    https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/attaching-files
+                    """
+                    raise PrecommitError(
+                        dedent(error_message).strip().replace("\n", " ")
+                    )
+
+
+def open_notebook(filename: str) -> dict:
+    return nbformat.read(filename, as_version=nbformat.NO_CONVERT)
 
 
 if __name__ == "__main__":
