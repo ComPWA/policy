e8f7e47b31d75023124054fb78d3adf29fb0df9f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2022-08-09 15:48:18 +0200

    FIX: switch to list of options

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-case": ["upper-case"],
+    "type-case": [2, "always", ["upper-case"]],
     "type-enum": [
       2,
       "always",
