15e879710087318628e858ccf23990106300e859
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-03-20 14:45:51 +0100

    DX: remove `travisillig.vscode-json-stable-stringify`

--- src/repoma/check_dev_files/vscode.py
+++ src/repoma/check_dev_files/vscode.py
@@ -1,11 +1,24 @@
 """Check configuration of VS Code."""
 
-from repoma.utilities.vscode import remove_unwanted_recommendations
+from repoma.utilities.executor import Executor
+from repoma.utilities.vscode import (
+    remove_unwanted_recommendations,
+    remove_vscode_extension_recommendation,
+)
 
 
 def main() -> None:
-    _update_extensions()
+    executor = Executor()
+    executor(_update_extensions)
+    executor.finalize()
 
 
 def _update_extensions() -> None:
-    remove_unwanted_recommendations()
+    executor = Executor()
+    executor(remove_unwanted_recommendations)
+    executor(
+        remove_vscode_extension_recommendation,
+        "travisillig.vscode-json-stable-stringify",
+        # cspell:ignore travisillig
+    )
+    executor.finalize()
