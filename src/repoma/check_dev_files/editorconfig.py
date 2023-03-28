f1a47c2c1347d905cd93095dac21abe2a157d6bd
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-03-28 16:02:18 +0200

    MAINT: switch logics of `editorconfig` existence check

--- src/repoma/check_dev_files/editorconfig.py
+++ src/repoma/check_dev_files/editorconfig.py
@@ -5,60 +5,75 @@ pre-commit hook
 <https://github.com/editorconfig-checker/editorconfig-checker.python>`_.
 """
 
-import os
+from functools import lru_cache
 from textwrap import dedent
 
-import yaml
+from ruamel.yaml.comments import CommentedMap, CommentedSeq
+from ruamel.yaml.scalarstring import FoldedScalarString
 
 from repoma.errors import PrecommitError
+from repoma.utilities import CONFIG_PATH
+from repoma.utilities.precommit import find_repo, load_round_trip_precommit_config
 
-__PRECOMMIT_CONFIG_FILE = ".pre-commit-config.yaml"
-__EDITORCONFIG_FILE = ".editorconfig"
 __EDITORCONFIG_URL = (
     "https://github.com/editorconfig-checker/editorconfig-checker.python"
 )
-__EDITORCONFIG_HOOK = Rf"""
-  - repo: {__EDITORCONFIG_URL}
-    rev: ""
-    hooks:
-      - id: editorconfig-checker
-        exclude: >
-          (?x)^(
-            .*\.py
-          )$
-"""
 
 
 def main() -> None:
-    if _has_editorconfig() and not _has_precommit_hook():
-        raise PrecommitError(
-            dedent(
-                f"""
-                This repository has an {__EDITORCONFIG_FILE} file, but its
-                {__PRECOMMIT_CONFIG_FILE} file contains no hook to enforce it.
-                Please add the following hook:
-                {__EDITORCONFIG_HOOK}
-                and run pre-commit autoupdate
-                """
-            ).strip()
-        )
+    if CONFIG_PATH.editorconfig.exists():
+        _update_precommit_config()
 
 
-def _has_editorconfig() -> bool:
-    if not os.path.exists(__EDITORCONFIG_FILE):
-        return False
-    return True
+def _update_precommit_config() -> None:
+    if not CONFIG_PATH.precommit.exists():
+        return
+    expected_hook = __get_expected_hook_definition()
+    existing_config, yaml = load_round_trip_precommit_config()
+    repos: CommentedSeq = existing_config.get("repos", [])
+    idx_and_repo = find_repo(existing_config, __EDITORCONFIG_URL)
+    if idx_and_repo is None:
+        repos.append(expected_hook)
+        idx = len(repos) - 1
+        repos.yaml_set_comment_before_after_key(idx, before="\n")
+        yaml.dump(existing_config, CONFIG_PATH.precommit)
+        raise PrecommitError(f"Added editorconfig hook to {CONFIG_PATH.precommit}")
+    idx, existing_hook = idx_and_repo
+    if not __is_equivalent(existing_hook, expected_hook):
+        existing_rev = existing_hook.get("rev")
+        if existing_rev is not None:
+            expected_hook["rev"] = existing_rev
+        repos[idx] = expected_hook
+        repos.yaml_set_comment_before_after_key(idx + 1, before="\n")
+        yaml.dump(existing_config, CONFIG_PATH.precommit)
+        raise PrecommitError(f"Updated editorconfig hook in {CONFIG_PATH.precommit}")
 
 
-def _has_precommit_hook() -> bool:
-    if not os.path.exists(__PRECOMMIT_CONFIG_FILE):
-        return False
-    with open(__PRECOMMIT_CONFIG_FILE) as stream:
-        config = yaml.load(stream, Loader=yaml.SafeLoader)
-    repos = config.get("repos")
-    if repos is None:
-        return False
-    repos_urls = {repo.get("repo") for repo in repos if isinstance(repo, dict)}
-    if __EDITORCONFIG_URL not in repos_urls:
-        return False
-    return True
+@lru_cache(maxsize=None)
+def __get_expected_hook_definition() -> CommentedMap:
+    excludes = R"""
+    (?x)^(
+      .*\.py
+    )$
+    """
+    excludes = dedent(excludes).strip()
+    hook = {
+        "id": "editorconfig-checker",
+        "name": "editorconfig",
+        "exclude": FoldedScalarString(excludes),
+    }
+    dct = {
+        "repo": __EDITORCONFIG_URL,
+        "rev": "",
+        "hooks": [CommentedMap(hook)],
+    }
+    return CommentedMap(dct)
+
+
+def __is_equivalent(expected: CommentedMap, existing: CommentedMap) -> bool:
+    def remove_rev(config: CommentedMap) -> dict:
+        config_copy = dict(config)
+        config_copy.pop("rev", None)
+        return config_copy
+
+    return remove_rev(expected) == remove_rev(existing)
