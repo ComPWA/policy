467bc74338468577d9d610d5d7ba15d69153aee8
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-07-07 00:06:25 +0200

    DX: recommend and configure VSCode mypy extension (#153)
    * FIX: ignore `jsonschema` deprecation warning
    * MAINT: autoupdate pre-commit hooks
    * MAINT: remove `extract_extensions_test`

--- tests/check_dev_files/test_gitpod.py
+++ tests/check_dev_files/test_gitpod.py
@@ -3,32 +3,6 @@ import pytest
 from repoma.check_dev_files.gitpod import _extract_extensions, _generate_gitpod_config
 
 
-def test_extract_extensions():
-    assert _extract_extensions() == [
-        # cspell:disable
-        "charliermarsh.ruff",
-        "christian-kohler.path-intellisense",
-        "davidanson.vscode-markdownlint",
-        "eamodio.gitlens",
-        "editorconfig.editorconfig",
-        "esbenp.prettier-vscode",
-        "garaioag.garaio-vscode-unwanted-recommendations",
-        "github.vscode-github-actions",
-        "github.vscode-pull-request-github",
-        "ms-python.python",
-        "ms-python.vscode-pylance",
-        "ms-vsliveshare.vsliveshare",
-        "redhat.vscode-yaml",
-        "ryanluker.vscode-coverage-gutters",
-        "stkb.rewrap",
-        "streetsidesoftware.code-spell-checker",
-        "tamasfe.even-better-toml",
-        "tyriar.sort-lines",
-        "yzhang.markdown-all-in-one",
-        # cspell:enable
-    ]
-
-
 @pytest.mark.parametrize("pin_dependencies", [False, True])
 def test_get_gitpod_content(pin_dependencies: bool):
     gitpod_content = _generate_gitpod_config(pin_dependencies)
