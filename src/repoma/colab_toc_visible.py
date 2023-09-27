835e561c137f9dfa2a61e242b35b1a0774bd05f4
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-09-27 15:08:42 +0200

    FEAT: add hook for making Colab TOC visible (#174)

---
+++ src/repoma/colab_toc_visible.py
@@ -0,0 +1,54 @@
+"""Add notebook metadata to open the TOC sidebar on Google Colab.
+
+See `ComPWA/repo-maintenance#40 <https://github.com/ComPWA/repo-maintenance/issues/40>`_
+for more information.
+"""
+
+import argparse
+import sys
+from typing import Optional, Sequence
+
+import nbformat
+from nbformat import NotebookNode
+
+from .errors import PrecommitError
+from .utilities.executor import Executor
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument(
+        "filenames",
+        nargs="*",
+        help="Paths to the notebooks of which the metadata should be updated.",
+    )
+    args = parser.parse_args(argv)
+    executor = Executor()
+    for filename in args.filenames:
+        executor(_update_metadata, filename)
+    return executor.finalize(exception=False)
+
+
+def _update_metadata(path: str) -> None:
+    notebook = open_notebook(path)
+    metadata = notebook["metadata"]
+    updated = False
+    if metadata.get("colab") is None:
+        updated = True
+        metadata["colab"] = {}
+    if not metadata["colab"].get("toc_visible"):
+        updated = True
+        metadata["colab"]["toc_visible"] = True
+    if not updated:
+        return
+    nbformat.write(notebook, path)
+    msg = f"Colab TOC is now visible for notebook {path}"
+    raise PrecommitError(msg)
+
+
+def open_notebook(path: str) -> NotebookNode:
+    return nbformat.read(path, as_version=nbformat.NO_CONVERT)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
