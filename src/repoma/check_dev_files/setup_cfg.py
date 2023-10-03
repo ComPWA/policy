03f90be43472eb6c2e281e613f35d14e7b698871
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-10-03 17:20:37 +0200

    FIX: correctly import substitution syntax

--- src/repoma/check_dev_files/setup_cfg.py
+++ src/repoma/check_dev_files/setup_cfg.py
@@ -1,18 +1,23 @@
 """Apply a certain set of standards to the :file:`setup.cfg`."""
-
+# pyright: reportUnknownLambdaType=false
+import dataclasses
 import os
+import re
 import textwrap
 from collections import defaultdict
-from typing import Union
+from configparser import RawConfigParser
+from typing import Dict, List, Tuple, Union
 
 import tomlkit
 from ini2toml.api import Translator
+from tomlkit import TOMLDocument
 from tomlkit.container import Container
-from tomlkit.items import Table
+from tomlkit.items import Array, Table
 
 from repoma.errors import PrecommitError
 from repoma.utilities import CONFIG_PATH
 from repoma.utilities.executor import Executor
+from repoma.utilities.project_info import get_pypi_name
 from repoma.utilities.pyproject import (
     get_sub_table,
     load_pyproject,
@@ -41,10 +46,63 @@ def _convert_to_pyproject() -> None:
     converted_cfg = tomlkit.parse(toml_str)
     pyproject = load_pyproject()
     _update_container(pyproject, converted_cfg)
+    extras_require = _get_recursive_optional_dependencies()
+    if extras_require:
+        _update_optional_dependencies(pyproject, extras_require)
     write_pyproject(pyproject)
     os.remove(setup_cfg)
     msg = f"Converted {setup_cfg} configuration to {CONFIG_PATH.pyproject}"
     raise PrecommitError(msg)
+
+
+def _get_recursive_optional_dependencies() -> Dict[str, List[Tuple[str, str]]]:
+    if not CONFIG_PATH.setup_cfg.exists():
+        return {}
+    cfg = RawConfigParser()
+    cfg.read(CONFIG_PATH.setup_cfg)
+    section_name = "options.extras_require"
+    if section_name not in cfg.sections():
+        return {}
+    return {
+        option: __extract_package_list(cfg.get(section_name, option, raw=True))
+        for option in cfg.options(section_name)
+    }
+
+
+def _update_optional_dependencies(
+    pyproject: TOMLDocument, extras_require: Dict[str, List[Tuple[str, str]]]
+) -> None:
+    package_name = get_pypi_name(pyproject)
+    optional_dependencies = tomlkit.table()
+    for key, packages in extras_require.items():
+        package_array = tomlkit.array()
+        for package, comment in packages:
+            package = re.sub(r"%\(([^\)]+)\)s", rf"{package_name}[\1]", package)
+            toml_str = tomlkit.string(package, escape=False, literal='"' in package)
+            package_array.append(toml_str)
+            if comment:
+                __add_comment(package_array, -1, comment)
+        package_array.multiline(True)
+        optional_dependencies[key] = package_array
+    project = get_sub_table(pyproject, "project", create=True)
+    project["optional-dependencies"] = optional_dependencies
+
+
+def __extract_package_list(raw_content: str) -> List[Tuple[str, str]]:
+    def split_comment(line: str) -> Tuple[str, str]:
+        if "#" in line:
+            return tuple(s.strip() for s in line.split("#", maxsplit=1))  # type: ignore[return-value]
+        return line.strip(), ""
+
+    raw_content = raw_content.strip()
+    return [split_comment(s) for s in raw_content.split("\n")]
+
+
+def __add_comment(array: Array, idx: int, comment: str) -> None:  # disgusting hack
+    toml_comment = tomlkit.comment(comment)
+    toml_comment.indent(1)
+    toml_comment._trivia = dataclasses.replace(toml_comment._trivia, trail="")
+    array._value[idx].comment = toml_comment
 
 
 def _update_container(
