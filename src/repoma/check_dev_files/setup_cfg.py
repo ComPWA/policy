2605659901bce7bc4307f8756c4d8fc289605966
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-10-08 15:05:53 +0200

    FIX: sort options in pre-commit error

--- src/repoma/check_dev_files/setup_cfg.py
+++ src/repoma/check_dev_files/setup_cfg.py
@@ -141,10 +141,10 @@ def _check_required_options() -> None:
         return
     required_options = {
         "project": [
-            "name",
+            "classifiers",
             "description",
             "license",
-            "classifiers",
+            "name",
             "requires-python",
         ],
     }
@@ -158,7 +158,7 @@ def _check_required_options() -> None:
         summary = "\n"
         for section, options in missing_options.items():
             summary += f"[{section}]\n...\n"
-            for option in options:
+            for option in sorted(options):
                 summary += f"{option} = ...\n"
             summary += "...\n"
         raise PrecommitError(
