cf04dc088b994550588b0c82c12eb36b3a7766de
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-12-03 12:21:05 +0100

    ENH: automatically sort pre-commit hooks (#235)
    * MAINT: remove redundant update step

--- src/repoma/check_dev_files/precommit.py
+++ src/repoma/check_dev_files/precommit.py
@@ -1,8 +1,7 @@
 """Check content of :code:`.pre-commit-config.yaml` and related files."""
 
 from pathlib import Path
-from textwrap import dedent
-from typing import List, Set
+from typing import Iterable, List, Optional, Set, Tuple
 
 from ruamel.yaml.comments import CommentedMap, CommentedSeq
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString
@@ -13,49 +12,40 @@ from repoma.utilities.executor import Executor
 from repoma.utilities.precommit import PrecommitConfig
 from repoma.utilities.yaml import create_prettier_round_trip_yaml
 
-__NON_SKIPPED_HOOKS = {
-    "editorconfig-checker",
-}
-
 
 def main() -> None:
     cfg = PrecommitConfig.load()
     executor = Executor()
-    executor(_check_plural_hooks_first, cfg)
-    executor(_check_single_hook_sorting, cfg)
-    executor(_check_skipped_hooks, cfg)
+    executor(_sort_hooks)
     executor(_update_conda_environment, cfg)
+    executor(_update_precommit_ci_skip, cfg)
     executor.finalize()
 
 
-def _check_plural_hooks_first(config: PrecommitConfig) -> None:
-    if config.ci is None:
+def _sort_hooks() -> None:
+    yaml = create_prettier_round_trip_yaml()
+    contents: CommentedMap = yaml.load(CONFIG_PATH.precommit)
+    repos: Optional[CommentedSeq] = contents.get("repos")
+    if repos is None:
         return
-    plural_hook_repos = [r for r in config.repos if len(r.hooks) > 1]
-    n_plural_repos = len(plural_hook_repos)
-    if config.repos[:n_plural_repos] != plural_hook_repos:
-        msg = (
-            "Please bundle repos with multiple hooks at the top of the pre-commit"
-            " config"
-        )
+    sorted_repos: List[CommentedMap] = sorted(repos, key=__repo_def_sorting)
+    contents["repos"] = sorted_repos
+    if sorted_repos != repos:
+        yaml.dump(contents, CONFIG_PATH.precommit)
+        msg = f"Sorted pre-commit hooks in {CONFIG_PATH.precommit}"
         raise PrecommitError(msg)
 
 
-def _check_single_hook_sorting(config: PrecommitConfig) -> None:
-    if config.ci is None:
-        return
-    single_hook_repos = [r for r in config.repos if len(r.hooks) == 1]
-    expected_repo_order = sorted(
-        (r for r in single_hook_repos),
-        key=lambda r: r.hooks[0].id,
-    )
-    if single_hook_repos != expected_repo_order:
-        msg = "Pre-commit hooks are not sorted. Should be as follows:\n\n  "
-        msg += "\n  ".join(f"{r.hooks[0].id:20s} {r.repo}" for r in expected_repo_order)
-        raise PrecommitError(msg)
+def __repo_def_sorting(repo_def: CommentedMap) -> Tuple[int, str]:
+    if repo_def["repo"] == "meta":
+        return (0, "meta")
+    hooks: CommentedSeq = repo_def["hooks"]
+    if len(hooks) > 1:
+        return 1, repo_def["repo"]
+    return (2, hooks[0]["id"])
 
 
-def _check_skipped_hooks(config: PrecommitConfig) -> None:
+def _update_precommit_ci_skip(config: PrecommitConfig) -> None:
     if config.ci is None:
         return
     local_hooks = get_local_hooks(config)
@@ -73,26 +63,21 @@ def _check_skipped_hooks(config: PrecommitConfig) -> None:
         return
     existing_skips = __get_precommit_ci_skips(config)
     if existing_skips != expected_skips:
-        yaml = create_prettier_round_trip_yaml()
-        contents = yaml.load(CONFIG_PATH.precommit)
-        ci_section: CommentedMap = contents["ci"]
-        if "skip" in ci_section.ca.items:
-            del ci_section.ca.items["skip"]
-        skips = CommentedSeq(sorted(expected_skips))
-        ci_section["skip"] = skips
-        contents.yaml_set_comment_before_after_key("repos", before="\n")
-        yaml.dump(contents, CONFIG_PATH.precommit)
-        msg = f"Updated ci.skip section in {CONFIG_PATH.precommit}"
-        raise PrecommitError(msg)
-    hooks_to_execute = __NON_SKIPPED_HOOKS & existing_skips
-    if hooks_to_execute:
-        msg = f"""
-        Please remove the following hooks from the ci.skip section of {CONFIG_PATH.precommit}:
+        __update_precommit_ci_skip(expected_skips)
 
-            {', '.join(sorted(hooks_to_execute))}
-        """
-        msg = dedent(msg)
-        raise PrecommitError(msg)
+
+def __update_precommit_ci_skip(expected_skips: Iterable[str]) -> None:
+    yaml = create_prettier_round_trip_yaml()
+    contents = yaml.load(CONFIG_PATH.precommit)
+    ci_section: CommentedMap = contents["ci"]
+    if "skip" in ci_section.ca.items:
+        del ci_section.ca.items["skip"]
+    skips = CommentedSeq(sorted(expected_skips))
+    ci_section["skip"] = skips
+    contents.yaml_set_comment_before_after_key("repos", before="\n")
+    yaml.dump(contents, CONFIG_PATH.precommit)
+    msg = f"Updated ci.skip section in {CONFIG_PATH.precommit}"
+    raise PrecommitError(msg)
 
 
 def __get_precommit_ci_skips(config: PrecommitConfig) -> Set[str]:
