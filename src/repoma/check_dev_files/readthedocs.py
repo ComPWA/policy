be09bca50c4b8b72a2ef955440f98697e2f4cc6f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-12-06 21:15:36 +0100

    DX: upgrade to `ubuntu-22.04` in Read the Docs config

--- src/repoma/check_dev_files/readthedocs.py
+++ src/repoma/check_dev_files/readthedocs.py
@@ -1,5 +1,7 @@
 """Update Read the Docs configuration."""
 
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from ruamel.yaml.scalarstring import DoubleQuotedScalarString
@@ -18,14 +20,31 @@ def main(python_version: PythonVersion) -> None:
     if not CONFIG_PATH.readthedocs.exists():
         return
     executor = Executor()
+    executor(_update_os)
     executor(_update_python_version, python_version)
     executor(_update_install_step, python_version)
     executor.finalize()
 
 
+def _update_os() -> None:
+    yaml = create_prettier_round_trip_yaml()
+    config: CommentedMap = yaml.load(CONFIG_PATH.readthedocs)
+    build: CommentedMap | None = config.get("build")
+    if build is None:
+        return
+    os: str | None = build.get("os")
+    expected = "ubuntu-22.04"
+    if os == expected:
+        return
+    build["os"] = expected
+    yaml.dump(config, CONFIG_PATH.readthedocs)
+    msg = f"Switched to {expected} in {CONFIG_PATH.readthedocs}"
+    raise PrecommitError(msg)
+
+
 def _update_python_version(python_version: PythonVersion) -> None:
     yaml = create_prettier_round_trip_yaml()
-    config = yaml.load(CONFIG_PATH.readthedocs)
+    config: CommentedMap = yaml.load(CONFIG_PATH.readthedocs)
     tools: CommentedMap = config.get("build", {}).get("tools", {})
     if tools is None:
         return
@@ -43,7 +62,7 @@ def _update_python_version(python_version: PythonVersion) -> None:
 
 def _update_install_step(python_version: PythonVersion) -> None:
     yaml = create_prettier_round_trip_yaml()
-    config = yaml.load(CONFIG_PATH.readthedocs)
+    config: CommentedMap = yaml.load(CONFIG_PATH.readthedocs)
     steps: CommentedSeq = config.get("build", {}).get("jobs", {}).get("post_install")
     if steps is None:
         return
