0436fc8503c90f678ee7fdf47ba7db4fb915009f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-12-06 22:15:20 +0100

    FIX: update pinned constraints in Conda environment

--- src/repoma/check_dev_files/conda.py
+++ src/repoma/check_dev_files/conda.py
@@ -1,10 +1,12 @@
 """Update the :file:`environment.yml` Conda environment file."""
 
-from pathlib import Path
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from repoma.errors import PrecommitError
-from repoma.utilities.project_info import PythonVersion
+from repoma.utilities import CONFIG_PATH
+from repoma.utilities.project_info import PythonVersion, get_constraints_file
 from repoma.utilities.yaml import create_prettier_round_trip_yaml
 
 if TYPE_CHECKING:
@@ -12,29 +14,58 @@ if TYPE_CHECKING:
 
 
 def main(python_version: PythonVersion) -> None:
-    _update_conda_environment(python_version)
-
-
-def _update_conda_environment(python_version: PythonVersion) -> None:
-    path = Path("environment.yml")
-    if not path.exists():
+    if not CONFIG_PATH.conda.exists():
         return
     yaml = create_prettier_round_trip_yaml()
-    conda_env: CommentedMap = yaml.load(path)
-    dependencies: CommentedSeq = conda_env.get("dependencies", [])
-    idx = None
+    conda_env: CommentedMap = yaml.load(CONFIG_PATH.conda)
+    conda_deps: CommentedSeq = conda_env.get("dependencies", [])
+
+    updated = _update_python_version(python_version, conda_deps)
+    updated |= _update_pip_dependencies(python_version, conda_deps)
+    if updated:
+        yaml.dump(conda_env, CONFIG_PATH.conda)
+        msg = f"Set the Python version in {CONFIG_PATH.conda} to {python_version}"
+        raise PrecommitError(msg)
+
+
+def _update_python_version(version: PythonVersion, conda_deps: CommentedSeq) -> bool:
+    idx = __find_python_dependency_index(conda_deps)
+    expected = f"python=={version}.*"
+    if idx is not None and conda_deps[idx] != expected:
+        conda_deps[idx] = expected
+        return True
+    return False
+
+
+def _update_pip_dependencies(version: PythonVersion, conda_deps: CommentedSeq) -> bool:
+    pip_deps = __get_pip_dependencies(conda_deps)
+    if pip_deps is None:
+        return False
+    constraints_file = get_constraints_file(version)
+    if constraints_file is None:
+        expected_pip = "-e .[dev]"
+    else:
+        expected_pip = f"-c {constraints_file} -e .[dev]"
+    if len(pip_deps) and pip_deps[0] != expected_pip:
+        pip_deps[0] = expected_pip
+        return True
+    return False
+
+
+def __find_python_dependency_index(dependencies: CommentedSeq) -> int | None:
     for i, dep in enumerate(dependencies):
         if not isinstance(dep, str):
             continue
         if dep.strip().startswith("python"):
-            idx = i
-            break
-    if idx is None:
-        return
-    expected = f"python=={python_version}.*"
-    if dependencies[idx] == expected:
-        return
-    dependencies[idx] = expected
-    yaml.dump(conda_env, path)
-    msg = f"Set the Python version in {path} to {python_version}"
-    raise PrecommitError(msg)
+            return i
+    return None
+
+
+def __get_pip_dependencies(dependencies: CommentedSeq) -> CommentedSeq | None:
+    for dep in dependencies:
+        if not isinstance(dep, dict):
+            continue
+        pip_deps = dep.get("pip")
+        if pip_deps is not None:
+            return pip_deps
+    return None
