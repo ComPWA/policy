a535c98f46ee915f8e133e74a48b2929ef948869
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2023-12-06 22:49:09 +0100

    FIX: update tests

--- tests/check_dev_files/test_gitpod.py
+++ tests/check_dev_files/test_gitpod.py
@@ -1,11 +1,8 @@
-import pytest
-
 from repoma.check_dev_files.gitpod import _extract_extensions, _generate_gitpod_config
 
 
-@pytest.mark.parametrize("pin_dependencies", [False, True])
-def test_get_gitpod_content(pin_dependencies: bool):
-    gitpod_content = _generate_gitpod_config(pin_dependencies)
+def test_get_gitpod_content():
+    gitpod_content = _generate_gitpod_config("3.8")
     assert set(gitpod_content) == {
         "github",
         "tasks",
@@ -22,12 +19,8 @@ def test_get_gitpod_content(pin_dependencies: bool):
             "pullRequestsFromForks": True,
         }
     }
-    if pin_dependencies:
-        assert gitpod_content["tasks"] == [
-            {"init": "pip install -c .constraints/py3.8.txt -e .[dev]"},
-        ]
-    else:
-        assert gitpod_content["tasks"] == [
-            {"init": "pip install -e .[dev]"},
-        ]
+    assert gitpod_content["tasks"] == [
+        {"init": "pyenv local 3.8"},
+        {"init": "pip install -e .[dev]"},
+    ]
     assert gitpod_content["vscode"]["extensions"] == _extract_extensions()
