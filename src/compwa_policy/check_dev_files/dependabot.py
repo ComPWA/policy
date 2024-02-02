55c66101d34f3550de359f0f96cd9896d0c3d23a
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-02-02 12:22:06 +0100

    BEHAVIOR: remove `dependabot.yml` config (#292)
    * DX: improve PR title of dependabot updates

---
+++ src/compwa_policy/check_dev_files/dependabot.py
@@ -0,0 +1,57 @@
+"""Remove :file:`dependabot.yml` file, or update it if requested."""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING
+
+from compwa_policy.errors import PrecommitError
+from compwa_policy.utilities import COMPWA_POLICY_DIR, CONFIG_PATH
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+if TYPE_CHECKING:
+    from pathlib import Path
+
+DependabotOption = Literal["keep", "update"]
+"""Allowed options for the :code:`--dependabot` argument."""
+
+
+def main(allow_dependabot: DependabotOption | None) -> None:
+    dependabot_path = CONFIG_PATH.github_workflow_dir.parent / "dependabot.yml"
+    if allow_dependabot is None:
+        _remove_dependabot(dependabot_path)
+    elif allow_dependabot == "update":
+        _update_dependabot(dependabot_path)
+
+
+def _remove_dependabot(dependabot_path: Path) -> None:
+    if not dependabot_path.exists():
+        return
+    dependabot_path.unlink()
+    msg = (
+        f"Removed {dependabot_path}, because it is GitHub workflows have been"
+        " outsourced to https://github.com/ComPWA/actions"
+    )
+    raise PrecommitError(msg)
+
+
+def _update_dependabot(dependabot_path: Path) -> None:
+    template_path = COMPWA_POLICY_DIR / dependabot_path
+    with open(template_path) as f:
+        template = f.read()
+    if not dependabot_path.exists():
+        __dump_dependabot_template(template, dependabot_path)
+    with open(dependabot_path) as f:
+        dependabot = f.read()
+    if dependabot != template:
+        __dump_dependabot_template(template, dependabot_path)
+
+
+def __dump_dependabot_template(content: str, path: Path) -> None:
+    with open(path, "w") as f:
+        f.write(content)
+    msg = f"Updated {path}"
+    raise PrecommitError(msg)
