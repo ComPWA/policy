716633a021dc4e8a9f49fc7568f6b88f53072b32
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-08-09 22:27:43 +0200

    ENH: support `pixi` and `venv` in `.envrc`

--- src/compwa_policy/check_dev_files/direnv.py
+++ src/compwa_policy/check_dev_files/direnv.py
@@ -2,29 +2,58 @@
 
 from __future__ import annotations
 
-from textwrap import dedent
+from textwrap import dedent, indent
 
 from compwa_policy.errors import PrecommitError
 from compwa_policy.utilities import CONFIG_PATH
 from compwa_policy.utilities.pyproject import Pyproject
+from compwa_policy.utilities.python import has_constraint_files
+
+__DIRENV_SCRIPTS = {
+    "conda": "layout anaconda",
+    "pixi": """
+        watch_file pixi.lock
+        eval "$(pixi shell-hook)"
+    """,
+    "venv": "source venv/bin/activate",
+    "uv-venv": "source .venv/bin/activate",
+    "uv-venv-update": """
+        uv pip sync .constraints/py$(python3 --version | awk '{print $2}' | awk -F. '{print $1"."$2}').txt --quiet
+        uv pip install --editable '.[dev]' --quiet
+    """,
+}
 
 
 def main() -> None:
+    uv_venv = __DIRENV_SCRIPTS["uv-venv"]
+    if has_constraint_files():
+        uv_venv += "\n" + dedent(__DIRENV_SCRIPTS["uv-venv-update"])
+    statements: list[tuple[str | None, str]] = [
+        (".venv", uv_venv),
+        ("venv", __DIRENV_SCRIPTS["venv"]),
+    ]
     if (
         CONFIG_PATH.pixi_lock.exists()
         or CONFIG_PATH.pixi_toml.exists()
         or (CONFIG_PATH.pyproject.exists() and Pyproject.load().has_table("tool.pixi"))
     ):
-        _update_envrc("""
-        watch_file pixi.lock
-        eval "$(pixi shell-hook)"
-        """)
-    elif CONFIG_PATH.conda.exists():
-        _update_envrc("layout anaconda")
+        statements.append((".pixi", __DIRENV_SCRIPTS["pixi"]))
+    if CONFIG_PATH.conda.exists():
+        statements.append((None, __DIRENV_SCRIPTS["conda"]))
+    _update_envrc(statements)
 
 
-def _update_envrc(expected: str) -> None:
-    expected = dedent(expected).strip() + "\n"
+def _update_envrc(statements: list[tuple[str | None, str]]) -> None:
+    expected = ""
+    for i, (trigger_path, script) in enumerate(statements):
+        if trigger_path is not None:
+            if_or_elif = "if" if i == 0 else "elif"
+            expected += f"{if_or_elif} [ -e {trigger_path} ]; then\n"
+        else:
+            expected += "else\n"
+        script = dedent(script).strip()
+        expected += indent(script, prefix="  ") + "\n"
+    expected += "fi\n"
     existing = __get_existing_envrc()
     if existing == expected:
         return
