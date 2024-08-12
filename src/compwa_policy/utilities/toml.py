df16d61f505565e93cf5158d86f35229f78e90fc
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-08-12 09:48:54 +0200

    ENH: allow enforce single-line arrays

--- src/compwa_policy/utilities/toml.py
+++ src/compwa_policy/utilities/toml.py
@@ -10,10 +10,15 @@ if TYPE_CHECKING:
     from tomlkit.items import Array
 
 
-def to_toml_array(items: Iterable[Any], multiline: bool = False) -> Array:
+def to_toml_array(items: Iterable[Any], multiline: bool | None = None) -> Array:
     array = tomlkit.array()
     array.extend(items)
-    if multiline or len(array) > 1:
+    if multiline is None:
+        if len(array) > 1:
+            array.multiline(True)
+        else:
+            array.multiline(False)
+    elif multiline:
         array.multiline(True)
     else:
         array.multiline(False)
