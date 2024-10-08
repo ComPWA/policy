4dc8f6e596f11710407c2d43d13847a6ef4f6580
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-10-08 10:40:08 +0200

    FIX: remove `skip` key if no expected hook skips (#403)

--- src/compwa_policy/check_dev_files/precommit.py
+++ src/compwa_policy/check_dev_files/precommit.py
@@ -112,15 +112,15 @@ def _update_precommit_ci_skip(precommit: ModifiablePrecommit) -> None:
     non_functional_hooks = get_non_functional_hooks(precommit.document)
     expected_skips = sorted(set(non_functional_hooks) | set(local_hooks))
     existing_skips = precommit_ci.get("skip")
-    if not expected_skips and existing_skips is not None:
-        del precommit_ci["skip"]
-        msg = "Removed redundant ci.skip section"
-        precommit.changelog.append(msg)
-    if existing_skips != expected_skips:
+    if existing_skips and existing_skips != expected_skips:
         precommit_ci["skip"] = sorted(expected_skips)
         yaml_config = cast(CommentedMap, precommit.document)
         yaml_config.yaml_set_comment_before_after_key("repos", before="\n")
         msg = "Updated ci.skip section"
+        precommit.changelog.append(msg)
+    if not expected_skips and existing_skips:
+        del precommit_ci["skip"]
+        msg = "Removed redundant ci.skip section"
         precommit.changelog.append(msg)
 
 
