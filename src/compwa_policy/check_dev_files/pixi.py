8322523fbdbd911f46ac01d50d1af318bcedd98f
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-10-14 09:09:14 +0200

    MAINT: remove Pixi from `.gitattributes`

--- src/compwa_policy/check_dev_files/pixi.py
+++ src/compwa_policy/check_dev_files/pixi.py
@@ -395,6 +395,7 @@ def __remove_pixi_configuration(pyproject: ModifiablePyproject) -> None:
     updated = False
     updated |= ___remove(CONFIG_PATH.pixi_lock)
     updated |= ___remove(CONFIG_PATH.pixi_toml)
+    updated |= __remove_from_git_attributes("pixi")
     if pyproject.has_table("tool.pixi"):
         del pyproject._document["tool"]["pixi"]  # pyright: ignore[reportTypedDictNotRequiredAccess] # noqa: SLF001
         updated = True
@@ -407,6 +408,22 @@ def ___remove(path: Path) -> bool:
     if not path.exists():
         return False
     path.unlink(missing_ok=True)
+    return True
+
+
+def __remove_from_git_attributes(word: str) -> bool:
+    if not CONFIG_PATH.gitattributes.exists():
+        return False
+    with open(CONFIG_PATH.gitattributes) as stream:
+        lines = stream.readlines()
+    filtered_lines = [line for line in lines if word not in line.lower()]
+    if not any(line.strip() for line in filtered_lines):
+        CONFIG_PATH.gitattributes.unlink()
+        return True
+    if len(filtered_lines) == len(lines):
+        return False
+    with open(CONFIG_PATH.gitattributes, "w") as stream:
+        stream.writelines(filtered_lines)
     return True
 
 
