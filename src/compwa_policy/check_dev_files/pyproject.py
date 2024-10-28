ce3ff89e30a507cba5b6d7ab69f1b927189dba68
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2024-10-28 09:10:21 +0100

    FEAT: convert dev dependencies to dependency groups

--- src/compwa_policy/check_dev_files/pyproject.py
+++ src/compwa_policy/check_dev_files/pyproject.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+import re
+
 from compwa_policy.utilities import CONFIG_PATH
 from compwa_policy.utilities.pyproject import ModifiablePyproject
 from compwa_policy.utilities.pyproject.getters import (
@@ -15,8 +17,49 @@ def main(excluded_python_versions: set[str], no_pypi: bool) -> None:
     if not CONFIG_PATH.pyproject.exists():
         return
     with ModifiablePyproject.load() as pyproject:
+        _convert_to_dependency_groups(pyproject)
         _update_requires_python(pyproject)
         _update_python_version_classifiers(pyproject, excluded_python_versions, no_pypi)
+
+
+def _convert_to_dependency_groups(pyproject: ModifiablePyproject) -> None:
+    table_key = "project.optional-dependencies"
+    if not pyproject.has_table(table_key):
+        return
+    optional_dependencies = pyproject.get_table(table_key)
+    dependency_groups = pyproject.get_table("dependency-groups", create=True)
+    dev_groups = {
+        "dev",
+        "doc",
+        "jupyter",
+        "lint",
+        "notebooks",
+        "sty",
+        "test",
+        "types",
+    }
+    package_name = pyproject.get_package_name()
+    updated = False
+    for group, dependencies in dict(optional_dependencies).items():
+        if group not in dev_groups:
+            continue
+        dependencies = [__convert_to_include(dep, package_name) for dep in dependencies]
+        dependency_groups[group] = to_toml_array(dependencies)
+        optional_dependencies.pop(group)
+        updated = True
+    if len(optional_dependencies) == 0:
+        del pyproject.get_table("project")["optional-dependencies"]
+    if updated:
+        msg = "Converted optional-dependencies to dependency-groups"
+        pyproject.changelog.append(msg)
+
+
+def __convert_to_include(dependency: str, package_name: str | None) -> str | dict:
+    if package_name is not None:
+        matches = re.match(rf"{package_name}\[(.+)\]", dependency)
+        if matches:
+            return {"include-group": matches.group(1)}
+    return dependency
 
 
 def _update_requires_python(pyproject: ModifiablePyproject) -> None:
