55da47ab6db4d68197d98ca7aa3b7784eb53a6bb
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2025-01-28 13:43:03 +0100

    ENH: add or remove `nbstripout` depending on `has_notebooks` (#505)

--- src/compwa_policy/check_dev_files/nbstripout.py
+++ src/compwa_policy/check_dev_files/nbstripout.py
@@ -12,52 +12,50 @@ if TYPE_CHECKING:
     from compwa_policy.utilities.precommit import ModifiablePrecommit
 
 
-def main(precommit: ModifiablePrecommit, allowed_cell_metadata: list[str]) -> None:
-    repo_url = "https://github.com/kynan/nbstripout"
-    repo = precommit.find_repo(repo_url)
-    if repo is None:
-        return
-    extra_keys_argument = {
-        "cell.attachments",
-        "cell.metadata.code_folding",
-        "cell.metadata.editable",
-        "cell.metadata.id",
-        "cell.metadata.pycharm",
-        "cell.metadata.slideshow",
-        "cell.metadata.user_expressions",
-        "metadata.celltoolbar",
-        "metadata.colab.name",
-        "metadata.colab.provenance",
-        "metadata.interpreter",
-        "metadata.notify_time",
-        "metadata.toc",
-        "metadata.toc-autonumbering",
-        "metadata.toc-showcode",
-        "metadata.toc-showmarkdowntxt",  # cspell:ignore showmarkdowntxt
-        "metadata.toc-showtags",
-        "metadata.varInspector",
-        "metadata.vscode",
-    }
-    extra_keys_argument -= {f"cell.metadata.{key}" for key in allowed_cell_metadata}
-    existing_hooks = repo["hooks"]
-    if existing_hooks:
-        args = existing_hooks[0].get("args", [])
-        if len(args) >= 3:  # noqa: PLR2004
-            existing_keys = {line.strip() for line in args[2].split("\n")}
-            existing_keys = {key for key in existing_keys if key}
-            extra_keys_argument.update(existing_keys)
-    expected_repo = Repo(
-        repo=repo_url,
-        rev="",
-        hooks=[
-            Hook(
-                id="nbstripout",
-                args=[
-                    "--drop-empty-cells",
-                    "--extra-keys",
-                    LiteralScalarString("\n".join(sorted(extra_keys_argument)) + "\n"),
-                ],
-            )
-        ],
-    )
-    precommit.update_single_hook_repo(expected_repo)
+def main(
+    precommit: ModifiablePrecommit,
+    has_notebooks: bool,
+    allowed_cell_metadata: list[str],
+) -> None:
+    if not has_notebooks:
+        precommit.remove_hook("nbstripout")
+    else:
+        extra_keys_argument = {
+            "cell.attachments",
+            "cell.metadata.code_folding",
+            "cell.metadata.editable",
+            "cell.metadata.id",
+            "cell.metadata.pycharm",
+            "cell.metadata.slideshow",
+            "cell.metadata.user_expressions",
+            "metadata.celltoolbar",
+            "metadata.colab.name",
+            "metadata.colab.provenance",
+            "metadata.interpreter",
+            "metadata.notify_time",
+            "metadata.toc",
+            "metadata.toc-autonumbering",
+            "metadata.toc-showcode",
+            "metadata.toc-showmarkdowntxt",  # cspell:ignore showmarkdowntxt
+            "metadata.toc-showtags",
+            "metadata.varInspector",
+            "metadata.vscode",
+        }
+        extra_keys_argument -= {f"cell.metadata.{key}" for key in allowed_cell_metadata}
+        expected_repo = Repo(
+            repo="https://github.com/kynan/nbstripout",
+            rev="",
+            hooks=[
+                Hook(
+                    id="nbstripout",
+                    args=[
+                        "--drop-empty-cells",
+                        "--extra-keys",
+                        LiteralScalarString(
+                            "\n".join(sorted(extra_keys_argument)) + "\n"
+                        ),
+                    ],
+                )
+            ],
+        )
+        precommit.update_single_hook_repo(expected_repo)
