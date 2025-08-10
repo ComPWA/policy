b3534624c0782fd28b465da3314df34c19af42fe
Remco de Boer <29308176+redeboer@users.noreply.github.com>
2025-08-10 16:17:06 +0200

    ENH: support Pixi as task runner

--- src/compwa_policy/check_dev_files/readthedocs.py
+++ src/compwa_policy/check_dev_files/readthedocs.py
@@ -12,7 +12,11 @@ from ruamel.yaml.scalarstring import DoubleQuotedScalarString, LiteralScalarStri
 from compwa_policy.errors import PrecommitError
 from compwa_policy.utilities import CONFIG_PATH, get_nested_dict
 from compwa_policy.utilities.match import filter_files
-from compwa_policy.utilities.pyproject import get_constraints_file
+from compwa_policy.utilities.pyproject import (
+    Pyproject,
+    get_constraints_file,
+    has_dependency,
+)
 from compwa_policy.utilities.yaml import create_prettier_round_trip_yaml
 
 if TYPE_CHECKING:
@@ -196,17 +200,26 @@ def __remove_nested_key(dct: dict, dotted_key: str) -> bool:
 
 def _update_build_step_for_pixi(config: ReadTheDocs) -> None:
     new_command = __get_pixi_install_statement() + "\n"
-    new_command += dedent(R"""
-        export UV_LINK_MODE=copy
-        pixi run \
-          uv run \
-            --group doc \
-            --no-dev \
-            --with poethepoet \
-            poe doc
-        mkdir -p $READTHEDOCS_OUTPUT
-        mv docs/_build/html $READTHEDOCS_OUTPUT
-    """).strip()
+    pyproject = Pyproject.load()
+    if has_dependency(pyproject, "poethepoet"):
+        new_command += dedent(R"""
+            export UV_LINK_MODE=copy
+            pixi run \
+            uv run \
+                --group doc \
+                --no-dev \
+                --with poethepoet \
+                poe doc
+            mkdir -p $READTHEDOCS_OUTPUT
+            mv docs/_build/html $READTHEDOCS_OUTPUT
+        """).strip()
+    else:
+        new_command += dedent(R"""
+            export UV_LINK_MODE=copy
+            pixi run doc
+            mkdir -p $READTHEDOCS_OUTPUT
+            mv docs/_build/html $READTHEDOCS_OUTPUT
+        """).strip()
     __update_build_step(
         config,
         new_command,
